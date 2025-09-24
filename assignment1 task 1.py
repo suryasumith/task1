884b9e048642027c2cb7428cebd44077787f949d
Surya <72338135+suryasumith@users.noreply.github.com>
2025-09-24 20:02:38 +0530

    Add files via upload

---
+++ assignment1 task 1.py
@@ -0,0 +1,25 @@
+#-----> taking input from user doing mathematical
+a=float(input("Enter The First Number   : "))
+b=float(input('Enter The Second Number   : '))
+
+#-----> Addition
+add= a + b
+print("Addition : ",add)
+#----> Subtraction
+sub = a - b
+print("Subtraction : ",sub)
+#----> Multiplication
+mul = a * b
+print("Multiplication : ",mul)
+#----> Division
+div = a / b
+print("Division : ",div)
+#----> Modulation
+mod = a % b
+print("Modulation : ",mod)
+
+print('Thank You Your Arithmatic was done\n')
+
+
+
+
---
+++ assignment1 task2.py
@@ -0,0 +1,6 @@
+#----> Taking input first name and last name as well as displaying
+print('Hello Python Programmer')
+fn=str(input('Pleas Enter The First Name : '))
+ln=str(input('Pleas Enter The Last Name : '))
+
+print('Hello',fn,ln,'Welcome to the python programing language')
